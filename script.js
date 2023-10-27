93582daa69eed8071c6d550f71fe6ae3f2c21042
Ganesh <4nm21cs063@nmamit.in>
2023-10-27 20:37:06 +0530

    added comment

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-function colorchange(){
-   var hex_num=["0","1","2","3","4","5","6","7","8","9","A","B","c","D","E","F"];
+function colorchange() {
+     var hex_num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "c", "D", "E", "F"];
+     // try
+     var hexcode = '';
+     for (var i = 0; i < 6; i++) {
+          var random = Math.floor(Math.random() * hex_num.length)
+          hexcode += hex_num[random];
 
-   var hexcode='';
-   for(var i=0;i<6;i++)
-   {
-        var random=Math.floor(Math.random()*hex_num.length)
-        hexcode+=hex_num[random];
-
-   }
-   document.getElementById("hex-code").innerHTML=hexcode;
-   document.getElementsByTagName("body")[0].style.background="#" + hexcode;
- }
+     }
+     document.getElementById("hex-code").innerHTML = hexcode;
+     document.getElementsByTagName("body")[0].style.background = "#" + hexcode;
+}
