d1cc84e64665b7bcfaab074d3323d2369614fe8e
Ganesh <4nm21cs063@nmamit.in>
2023-10-27 20:17:47 +0530

    initialcommit

---
+++ script.js
@@ -0,0 +1,13 @@
+function colorchange(){
+   var hex_num=["0","1","2","3","4","5","6","7","8","9","A","B","c","D","E","F"];
+
+   var hexcode='';
+   for(var i=0;i<6;i++)
+   {
+        var random=Math.floor(Math.random()*hex_num.length)
+        hexcode+=hex_num[random];
+
+   }
+   document.getElementById("hex-code").innerHTML=hexcode;
+   document.getElementsByTagName("body")[0].style.background="#" + hexcode;
+ }
